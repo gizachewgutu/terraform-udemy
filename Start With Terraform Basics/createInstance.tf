a03af7e32c05fc59254a0e8f61abba42b9a5519e
Jane <66804912+gizachewgutu@users.noreply.github.com>
2023-04-23 01:02:30 -0500

    Update createInstance.tf

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,10 +1,12 @@
+provider "aws" {
+  access_key = "AKIAQGTADVRMEK7WY6FY"
+  secret_key = "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"
+  region     = "us-east-1"
+  version = "3.53.0"
+}
 
 resource "aws_instance" "MyFirstInstnace" {
   count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
-
-  tags = {
-    Name = "demoinstnce-${count.index}"
-  }
+  ami           = "ami-0ac59804e0c10a625"
+  instance_type = "t2.micro"
 }
